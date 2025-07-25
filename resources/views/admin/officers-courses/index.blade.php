6ac845ef68c2460766d550faad7e42d435946702
gluis98 <luisfgb919@gmail.com>
2025-07-25 00:10:37 -0400

    Avances

--- resources/views/admin/officers-courses/index.blade.php
+++ resources/views/admin/officers-courses/index.blade.php
@@ -55,7 +55,7 @@
                         <div class="row">
                             <div class=" col-md-12 mb-3">
                                 <label class="form-label" for="descripcion">Descripción</label>
-                                <textarea class="form-control" id="descripcion" name="descripcion" required></textarea>
+                                <textarea class="form-control" id="descripcion" name="descripcion"></textarea>
                             </div>
                         </div>
                     </div>
